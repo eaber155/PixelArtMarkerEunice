3e73f32d968b66acfd3f88c8735cc3b753e087bc
Eunice Patricia Aber <eaber155@gmail.com>
2018-06-30 17:21:45 +0300

    The grid works now

--- designs.js
+++ designs.js
@@ -2,11 +2,12 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-document.body.style.backgroundImage = 'url("background_one.jpg")';
+document.body.style.backgroundImage = 'url("images/background_one.jpg")';
 
 window.onload = function(){
 	function changeBackgroundImage(){
-		var image = ["background_two.jpg", "background_three.jpg", "background_four.png", "background_five.jpg", "background_six.jpg"];
+		var image = ["images/background_two.jpg", "images/background_three.jpg", "images/background_four.png", "images/background_five.jpg", 
+		"images/background_six.jpg"];
 		var i = Math.floor(Math.random()*5);
 		document.body.style.backgroundImage = 'url("' + image[i] + '")';
 	}
@@ -18,36 +19,55 @@ window.onload = function(){
 
 $('#sizePicker').submit(function(event){
 	event.preventDefault();
-	var inputHeight = ('#inputHeight').val();
-	var inputWeight = ('#inputWeight').val();
-	makeGrid(inputHeight, inputWeight);
+	const height = $('#inputHeight').val();
+    const width = $('#inputWidth').val();
+	makeGrid(height, width);
 });
 
 
-/**$('#removeGrid').on('click', function(event){
-	event.preventDefault();
+function makeGrid(x, y) {
+	var gridMarkup = "";
 
-	$('<button>Love</button>').isAfter($("removeGrid"));
-});**/
-
-
-function makeGrid(inputHeight, inputWeight) {
-	var inputHeight, inputWeight, submit;
-
-	//inputHeight = ('#inputHeight');
-	//inputWeight = ('#inputWeight');
-
-	var gridMarkUp = "";
-
-	for(x=0; x<inputHeight;x++){
-		gridMarkUp += "<tr>";
-		for(y=0; y<inputWeight; y++){
-			gridMarkUp += "<td>&nbsp;</td>";
+	for (var i = 0; i < x; i++) {
+		gridMarkup += "<tr>";
+		for (k = 0; k <y; k++) {
+			gridMarkup += "<td>&nbsp;</td>";
 		}
-		gridMarkUp += "</tr>";
+		gridMarkup += "</tr>";	
 	}
 
-	$("#pixelCanvas").html(gridMarkUp);
+	$("#pixelCanvas").html(gridMarkup)
 }
+
+/**var height, width, color;
+
+$('#pixelCanvas').empty();
+   event.preventDefault();
+   const height = $('#inputHeight').val();
+   const width = $('#inputWidth').val();
+   makeGrid(height, width);
+})
+
+function makeGrid(x, y) {
+
+ // Your code goes here!
+ for (var i = 1; i <= x; i++) {
+     $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
+     for (var j = 1; j <=y; j++) {
+         $('#table' + i).append('<td></td>');
+       }
+   }
+   // add colors to the cells
+   $('td').click(function addColor(){
+       color = $('#colorPicker').val();
+
+       if ($(this).attr('style')) {
+           $(this).removeAttr('style')
+       } else {
+           $(this).attr('style', 'background-color:' + color);
+       }
+   })
+
+}*/
 
 //document.body.style.backgroundImage = 'url("background_one.jpg")';
